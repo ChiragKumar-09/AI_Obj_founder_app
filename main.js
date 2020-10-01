14f487708a94af02c2f2d0419993924a13d29479
ChiragKumar-09 <71137952+ChiragKumar-09@users.noreply.github.com>
2020-10-01 20:36:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 status = "";
 object_name = "";
+objects = [];
 
 function setup() {
     canvas = createCanvas(480 , 380);
@@ -12,16 +13,57 @@ function setup() {
 }
 function draw() {
     image(video , 0 , 0 , 480 , 380);
+
+    if (status != "") {
+        objectDetector.detect(video , gotResult);
+
+        for (let i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "STATUS : OBJECTS DETECTED";
+
+            fill("#FF1493");
+            percent = objects[i].confidence * 100 ;
+            text(objects[i].label+" "+percent+"%" , objects[i].x + 15 , bjects[i].y + 15);
+            noFill();
+            stroke("#FF1493");
+            rect(bjects[i].x , bjects[i].y , bjects[i].width , bjects[i].height);
+
+            if (objects[i].label == object_name) {
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("yes_no").innerHTML = object_name.toUpperCase()+" FOUND";
+                var synth = window.speechSynthesis;
+                speak_data = object_name;
+                utterThis = new SpeechSynthesisUtterance(speak_data);
+                synth.speak(utterThis);
+            } else {
+                document.getElementById("yes_no").innerHTML = object_name.toUpperCase()+" NOT FOUND";
+            }
+        }
+    }
 }
 
 function start() {
-    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
-    document.getElementById("status").innerHTML = "STATUS : DETECTING OBJECTS";
-    object_name = document.getElementById("object_name").value;
-    console.log(object_name);
+    if (document.getElementById("object_name").value != "") {
+        objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+        document.getElementById("status").innerHTML = "STATUS : DETECTING OBJECTS";
+        object_name = document.getElementById("object_name").value;
+        console.log(object_name);  
+    } else {
+        document.getElementById("object_name").placeholder = "Please Enter a Object !";
+    }
+
 }
 
 function modelLoaded() {
   console.log("model is loaded");
   status = true;
+}
+
+function gotResult(error , results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        objects = results;
+    }
 }
